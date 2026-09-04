b1be12c6d9ea6b97b6347f072800c3cb63edfffd
@honeysingh.397573 <honeysingh.397573@gmail.com>
2026-09-04 14:38:06 +0530

    new branches

--- python.py/error.py
+++ python.py/error.py
@@ -2,17 +2,18 @@ a = 8
 
 b = input("Enter the number\n")
 try:
-
     c = a/int(b)
 
     print(c)
 except ZeroDivisionError:
     print("You can not divide the number by zero")
+
 except ValueError:
     print("You can not divide the number by string")
+
 except Exception as e:
     print(e)
+
 except:
     print("Something went wrong")
 
-  
